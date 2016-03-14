10471bc47543d148ae23a77d0a14a07abee479ce
karan adep <karan.adep@cloudwick.com>
2016-03-13 23:06:36 -0700

    hot fix

--- installation_hadoop_daemons.sh
+++ installation_hadoop_daemons.sh
@@ -23,32 +23,32 @@ do
 case $var2 in
 	hdfs_namenode)
 			echo -e  "${var1}hdfs_namenode\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-hdfs-namenode' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-hdfs-namenode' < /dev/null
       			;;
         hdfs_secondary_namenode)
                         echo -e "${var1}hdfs_secondary_namenode\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-hdfs-secondarynamenode' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-hdfs-secondarynamenode' < /dev/null
                         ;;
         resource_manager)
                         echo -e "${var1}resource_manager\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-yarn-resourcemanager' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-yarn-resourcemanager' < /dev/null
 			''
                         ;;
         mapreduce_history_server)
                         echo -e "${var1}mapreduce_history_server\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-mapreduce-historyserver' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-mapreduce-historyserver' < /dev/null
                         ;;
         yarn_node_manager)
                         echo -e "${var1}yarn_node_manager\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-yarn-nodemanager' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-yarn-nodemanager' < /dev/null
                         ;;
         hdfs_datanode)
                         echo -e "${var1}hdfs_datanode\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-hdfs-datanode' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-hdfs-datanode' < /dev/null
                         ;;
         mapreduce)
                         echo -e "${var1}mapreduce\n"
-			ssh root@${var1} 'sudo yum clean all; sudo yum install -y hadoop-mapreduce' < /dev/null
+			ssh root@${var1} 'yum clean all; yum install -y hadoop-mapreduce' < /dev/null
                         ;;
         client)
                         echo -e "${var1}client\n"
